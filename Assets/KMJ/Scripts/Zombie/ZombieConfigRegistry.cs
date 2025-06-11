36365372dd855b7da78626ead599f01989644222
Min-Jun-Kang <zhfeps123@naver.com>
2025-06-11 10:25:27 +0900

    [Add] ZombieWave, AIDirector

---
+++ Assets/KMJ/Scripts/Zombie/ZombieConfigRegistry.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class ZombieConfigRegistry
+{
+    private static readonly List<ZombieConfig> list = new();
+    private static readonly Dictionary<ZombieConfig, int> map = new();
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void BuildTable()
+    {
+        list.Clear(); map.Clear();
+        var configs = Resources.LoadAll<ZombieConfig>("");
+        for (int i = 0; i < configs.Length; i++)
+        {
+            list.Add(configs[i]);
+            map[configs[i]] = i;  // ID = index
+        }
+        Debug.Log($"[Registry] Zombie configs loaded: {list.Count}");
+    }
+
+    public static int GetId(ZombieConfig cfg) => map[cfg];
+    public static ZombieConfig GetConfig(int id) =>
+        id >= 0 && id < list.Count ? list[id] : null;
+}
