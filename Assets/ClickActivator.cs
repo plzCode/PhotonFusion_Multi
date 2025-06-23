b6483c3244a4a0869f8de3fc2982fb7779fd0fa4
tkdtn404 <92982995+tkdtn404@users.noreply.github.com>
2025-06-23 23:21:24 +0900

    LightController light Active(false) branches of main

---
+++ Assets/ClickActivator.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class ClickActivator : MonoBehaviour
+{
+    //public LightController lightController;  // LightController 연결
+    //private bool isPlayerInRange = false;
+
+    //void Update()
+    //{
+    //    if (isPlayerInRange && Input.GetKeyDown(KeyCode.F))
+    //    {
+    //        Debug.Log("장치 근처에서 F 키 입력됨");
+    //        lightController.TurnOnLights();
+    //    }
+    //}
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        isPlayerInRange = true;
+    //        Debug.Log("플레이어 장치 범위 안에 들어옴");
+    //    }
+    //}
+
+    //void OnTriggerExit(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        isPlayerInRange = false;
+    //        Debug.Log("플레이어 장치 범위 밖으로 나감");
+    //    }
+    //}
+}
