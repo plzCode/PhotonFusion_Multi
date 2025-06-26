b01a22a2f071c18dc765fec85dbc5b85df0c2fef
JEL666 <dlawodms12@gmail.com>
2025-06-26 15:52:18 +0900

    [Fix] LobbySceneCamController

--- Assets/Scripts/Visual/LobbySceneCamController.cs
+++ Assets/Scripts/Visual/LobbySceneCamController.cs
@@ -56,7 +56,6 @@ public class LobbySceneCamController : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else if (Instance != this)
         {
